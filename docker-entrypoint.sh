e0c7b2acc0b788c4e79d52969fa13e5f16c7663a
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2021-06-04 01:31:29 +0900

    Add script for docker

---
+++ docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+main() {
+    # run rails server
+    cd /rask
+    rails s -b 0.0.0.0 -p 3000 &
+
+    pid=$!
+
+    # stop server when receive SIGTERM
+    trap 'signal_handler $pid' SIGTERM
+
+    # wait for daemon stopping
+    wait
+}
+
+signal_handler() {
+    echo "Stopping daemon pid: $1"
+    kill -2 $1
+}
+
+main
